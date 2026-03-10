0728356a712503bd976e168b0f91530f66fe6664
Brendon Wallace <brendonwallace2001@gmail.com>
2026-03-10 09:57:35 -0300

    deploy

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -1,9 +1,5 @@
-import type { Metadata } from 'next';
 import HomePage from './_components/home-page';
 
-export const metadata: Metadata = {
-  title: 'Boas vindas!',
-};
 
 export default async function Page() {
   return <HomePage />;
